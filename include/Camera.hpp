98c720faa77abdbcd0134b329b14affbede803a2
Jarrett Johnson <jarrettsjohnson@gmail.com>
2019-05-28 12:08:04 -0400

    include camera class

---
+++ include/Camera.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Camera
+{
+public:
+  glm::mat4 view() const { return glm::lookAt(m_pos, m_pos + dir(), up); }
+  glm::vec3 position() const { return m_pos; }
+  void setDir(glm::vec3 d) { m_dir = d; }
+  glm::vec3 dir() const { return m_dir; }
+  void translate(glm::vec3 trans) { m_pos += trans; }
+  static constexpr glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+private:
+  glm::vec3 m_pos = glm::vec3(0.0f, 0.0f, 3.0f);
+  glm::vec3 m_tar = glm::vec3(0.0f, 0.0, 0.0f);
+  glm::vec3 m_dir = glm::vec3(0.0f, 0.0f, -1.0f);
+};
